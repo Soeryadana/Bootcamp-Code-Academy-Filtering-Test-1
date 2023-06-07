edeff62a5427a92d5c96cfbfa751673b182e3387
I Dewa Gede Bagus Suryadana <bsuryadana@gmail.com>
2023-06-07 16:07:00 +0800

    update answer 07

--- 07-CompareArray.js
+++ 07-CompareArray.js
@@ -1,20 +1,28 @@
 function compareArray(array1, array2) {
     const same = [];
     const different = [];
+    const processed = [];
 
     array1.forEach((item) => {
-        if (array2.includes(item) && !same.includes(item)) {
+        if (processed.includes(item)) {
+            return;
+        }
+
+        if (array2.includes(item)) {
             same.push(item);
-        } else if (!different.includes(item)) {
+
+            const index = array2.indexOf(item);
+            array2.splice(index, 1);
+
+            processed.push(item);
+        } else {
             different.push(item);
+
+            processed.push(item);
         }
     });
 
-    array2.forEach((item) => {
-        if (!array1.includes(item) && !different.includes(item)) {
-            different.push(item);
-        }
-    });
+    different.push(...array2);
 
     return {
         same,
@@ -22,13 +30,12 @@ function compareArray(array1, array2) {
     };
 }
 
-const array1 = ["Mangga","Apel","Melon","Pisang","Sirsak","Tomat","Nanas","Nangka","Timun","Mangga"]
-const array2 = ["Bayam","Wortel","Kangkung","Mangga","Tomat","Kembang Kol", "Nangka","Timun"]
+const array1 = ["Mangga","Apel","Melon","Pisang","Sirsak","Tomat","Nanas","Nangka","Timun","Mangga"];
+const array2 = ["Bayam","Wortel","Kangkung","Mangga","Tomat","Kembang Kol", "Nangka","Timun"];
 
 const result = compareArray(array1, array2);
 console.log("Same = " + result.same);
 console.log("Different = " + result.different);
-
 
 // Result :
 //Same = ["Mangga","Tomat","Nangka","Timun"]
